c5731e4c5e3cefc3fed472715b0f2d488a5abf49
LizettePerez <lizette.perezcorrea@gmail.com>
2023-12-17 16:10:12 -0300

    componente categorias

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Header from '../components/header.jsx';
 import HeaderSection from '../components/HeaderSection.jsx';
+import Categorias from '../components/Categorias.jsx';
 
 
 function Home() {
@@ -7,6 +8,7 @@ function Home() {
     <>
       <Header />
       <HeaderSection />
+      <Categorias />
     </>
   );
 }
